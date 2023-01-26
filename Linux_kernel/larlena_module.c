28bf1f39d457dec369231ce9c00f097e9b6a9fda
larlena <azat251061@gmail.com>
2023-01-26 11:17:32 -0500

    scull_drivers now is pointer and it's allocating

--- Linux_kernel/larlena_module.c
+++ Linux_kernel/larlena_module.c
@@ -12,6 +12,15 @@
 	// #include <linux/sched.h>
 	// #include <asm/current.h>
 //
+
+#ifndef SCULL_NR_DEVS
+# define SCULL_NR_DEVS 1
+#endif
+
+#ifndef SCULL_MAJOR
+# define SCULL_MAJOR 0
+#endif
+
 MODULE_LICENSE("GPL");
 
 struct ft_scull_dev {
@@ -21,7 +30,18 @@ struct ft_scull_dev {
 	unsigned int		access_key;
 	struct semaphore	sem;
 	struct cdev		cdev;
-};
+} ;
+
+static struct file		*filp;
+static struct ft_scull_dev	*scull_devices;
+
+static unsigned int	scull_major = SCULL_MAJOR;
+static unsigned int	scull_minor = 0;
+static unsigned int	scull_nr_devs = SCULL_NR_DEVS;
+// module_param(scull_major, ulong, S_IRUGO);
+// module_param(scull_minor, ulong, S_IRUGO);
+// module_param(scull_nr_devs, ulong, S_IRUGO);
+
 
 static struct file_operations	scull_fops; /* = {
 	.owner		= THIS_MODULE,
@@ -32,15 +52,6 @@ static struct file_operations	scull_fops; /* = {
 	.open		= ft_open,
 	.release	= ft_release,
 } ;*/
-static struct file		*filp;
-static struct ft_scull_dev	scull_devices;
-
-static unsigned int	scull_major = 0;
-static unsigned int	scull_minor = 0;
-static unsigned int	scull_nr_devs = 1;
-// module_param(scull_major, ulong, 0644);
-// module_param(scull_minor, ulong, 0644);
-// module_param(scull_nr_devs, ulong, 0644);
 
 static void __init ft_scull_setup_cdev(struct ft_scull_dev *dev, int index) {
 	int	devno = MKDEV(scull_major, scull_minor + index);
@@ -56,8 +67,9 @@ static void __init ft_scull_setup_cdev(struct ft_scull_dev *dev, int index) {
 static int __init ft_larlenas_driver_init(void) {
 	dev_t	dev = 0;
 	int 	ret_value = 0;
+	size_t	i = 0;
 
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
 	if (scull_major) {
 		dev = MKDEV(scull_major, scull_minor);
 		ret_value = register_chrdev_region(dev, scull_nr_devs,"ft_scull");
@@ -67,22 +79,38 @@ static int __init ft_larlenas_driver_init(void) {
 	}
 	if (ret_value < 0) {
 		printk(KERN_WARNING "some_chrdev_region failed\n");
-		goto some_chardev_region_failed;
+		goto init_some_chardev_region_failed;
 	}
-	memset(&scull_devices, 0, sizeof(scull_devices));
-	ft_scull_setup_cdev(&scull_devices, 0);
+	if (!(scull_devices = kmalloc(scull_nr_devs * sizeof(*scull_devices), GFP_KERNEL))) {
+		printk(KERN_WARNING "kmalloc() faild\n");
+		goto init_kmalloc_faild;
+	}
+	memset(scull_devices, 0, scull_nr_devs * sizeof(*scull_devices));
+	while (i < scull_nr_devs) {
+		ft_scull_setup_cdev(&scull_devices[i], 0);
+		++i;
+	}
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
 
 	return 0;
-some_chardev_region_failed :
+init_kmalloc_faild :
+	unregister_chrdev_region(dev, scull_nr_devs);
+init_some_chardev_region_failed :
 	return ret_value;
 }
 
 static void __exit ft_larlenas_driver_exit(void) {
 	dev_t	devno = MKDEV(scull_major, scull_minor);
+	size_t	i = 0;
 
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
-	cdev_del(&scull_devices.cdev);
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
 	unregister_chrdev_region(devno, scull_nr_devs);
+	while (i < scull_nr_devs) {
+		cdev_del(&scull_devices[i].cdev);
+		++i;
+	}
+	kfree(scull_devices);
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
 }
 
 module_init(ft_larlenas_driver_init);
