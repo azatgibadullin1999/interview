363a81ec810abfb09228e5af6c2f5b7c6156fee3
larlena <azat251061@gmail.com>
2023-01-27 07:19:16 -0500

    added open release write read functions

--- Linux_kernel/larlena_module.c
+++ Linux_kernel/larlena_module.c
@@ -7,6 +7,8 @@
 #include <linux/init.h>
 #include <linux/cdev.h>
 #include <linux/slab.h>
+#include <asm/uaccess.h>
+#include <linux/mutex.h>
 
 // TESTING LIBRARY 
 	// #include <linux/sched.h>
@@ -30,16 +32,21 @@
 
 MODULE_LICENSE("GPL");
 
+struct ft_scull_qset {
+	void			**data;
+	struct ft_scull_qset	*next;
+} ;
+
 struct ft_scull_dev {
+	struct ft_scull_qset	*data;
 	unsigned int		quantum;
 	unsigned int		qset;
 	unsigned long		size;
 	unsigned int		access_key;
-	struct semaphore	sem;
+	struct mutex		mutex;
 	struct cdev		cdev;
 } ;
 
-static struct file		*filp;
 static struct ft_scull_dev	*scull_devices;
 
 static unsigned int	scull_major = SCULL_MAJOR;
@@ -52,19 +59,192 @@ module_param(scull_nr_devs, uint, S_IRUGO);
 module_param(scull_quantum, uint, S_IRUGO);
 module_param(scull_qset, uint, S_IRUGO);
 
+static struct ft_scull_qset	*ft_follow(struct ft_scull_dev *dev, int n) {
+	struct ft_scull_qset	*qs = dev->data;
 
-static struct file_operations	scull_fops; /* = {
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	if (!qs) {
+		if (!(qs = dev->data = kmalloc(sizeof(*qs), GFP_KERNEL))) {
+			return NULL;
+		}
+		memset(qs, 0, sizeof(*qs));
+	}
+	while (n--) {
+		if (!qs->next) {
+			qs->next = kmalloc(sizeof(*qs), GFP_KERNEL);
+			if (qs->next == NULL) {
+				return NULL;
+			}
+			memset(qs->next, 0, sizeof(*qs));
+		}
+		qs = qs->next;
+	}
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return qs;
+}
+
+static ssize_t	ft_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+	struct ft_scull_dev	*dev = filp->private_data;
+	struct ft_scull_qset	*dptr;
+	int	quantum = dev->quantum, qset = dev->qset;
+	int	itemsize = quantum * qset;
+	int	item, s_pos, q_pos, rest;
+	ssize_t	ret_value = -ENOMEM;
+
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return -ERESTARTSYS;
+	}
+	printk(KERN_ALERT "mutex is ok\n");
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
+
+	dptr = ft_follow(dev, item);
+	printk(KERN_ALERT "ft_follow is ok\n");
+	if (dptr == NULL) {
+		goto ft_write_out;
+	}
+	if (!dptr->data) {
+		dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
+		if (!dptr->data) {
+			goto ft_write_out;
+		}
+		memset(dptr->data, 0, qset * sizeof(char *));
+	}
+	if (!dptr->data[s_pos]) {
+		dptr->data[s_pos] = kmalloc(quantum, GFP_KERNEL);
+		if (!dptr->data[s_pos]) {
+			goto ft_write_out;
+		}
+	}
+	if (count > quantum - q_pos) {
+		count = quantum - q_pos;
+	}
+	if (copy_from_user(dptr->data[s_pos] + q_pos, buf, count)) {
+		ret_value = -EFAULT;
+		goto ft_write_out;
+	}
+	*f_pos += count;
+	ret_value = count;
+
+	if (dev->size < *f_pos) {
+		dev->size = *f_pos; 
+	}
+ft_write_out :
+	mutex_unlock(&dev->mutex);
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return ret_value;
+}
+
+ssize_t	ft_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
+	struct ft_scull_dev	*dev = filp->private_data;
+	struct ft_scull_qset	*dptr;
+	int	quantum = dev->quantum, qset = dev->qset;
+	int	itemsize = quantum * qset;
+	int	item, s_pos, q_pos, rest;
+	ssize_t	ret_value = 0;
+
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return -ERESTARTSYS;
+	}
+	if (*f_pos >= dev->size) {
+		goto ft_read_out;
+	}
+	if (*f_pos + count > dev->size) {
+		count = dev->size - *f_pos;
+	}
+
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
+
+	dptr = ft_follow(dev, item);
+
+	if (dptr == NULL ||  !dptr->data || !dptr->data[s_pos]) {
+		goto ft_read_out;
+	}
+	if (count > quantum - q_pos) {
+		count = quantum - q_pos;
+	}
+	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
+		ret_value = -EFAULT;
+		goto ft_read_out;
+	}
+	*f_pos += count;
+	ret_value = count;
+
+ft_read_out :
+	mutex_unlock(&dev->mutex);
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return ret_value;
+}
+
+static int	ft_trim(struct ft_scull_dev *dev) {
+	struct ft_scull_qset *next, *dptr = dev->data;
+	int	qset = dev->qset;
+	size_t	i;
+
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	while (dptr) {
+		if (dptr->data) {
+			for (i = 0; i < qset; ++i) {
+				kfree(dptr->data[i]);
+			}
+			kfree(dptr->data);
+			dptr->data = NULL;
+		}
+		next = dptr->next;
+		kfree(dptr);
+		dptr = next;
+	}
+	dev->size = 0;
+	dev->quantum = scull_quantum;
+	dev->qset = scull_qset;
+	dev->data = NULL;
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return 0;
+}
+
+int	ft_open(struct inode *inode, struct file *filp) {
+	struct ft_scull_dev	*dev;
+
+
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	dev = container_of(inode->i_cdev, struct ft_scull_dev, cdev);
+	filp->private_data = dev;
+	if ((filp->f_flags & O_ACCMODE) == O_WRONLY) {
+		if (mutex_lock_interruptible(&dev->mutex)) {
+			return -ERESTARTSYS;
+		}
+		ft_trim(dev);
+		mutex_unlock(&dev->mutex);
+	}
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return 0;
+}
+
+int	ft_release(struct inode *inode, struct file *filp) {
+	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "End the %s function\n", __FUNCTION__);
+	return 0;
+}
+
+static struct file_operations	scull_fops = {
 	.owner		= THIS_MODULE,
-	.llseek		= ft_llseek,
+	// .llseek		= ft_llseek,
 	.read		= ft_read,
 	.write		= ft_write,
-	.ioctl		= ft_ioctl,
+	// .ioctl		= ft_ioctl,
 	.open		= ft_open,
 	.release	= ft_release,
-} ;*/
+} ;
 
 static void __init ft_scull_setup_cdev(struct ft_scull_dev *dev, int index) {
-	int	devno = MKDEV(scull_major, scull_minor + index);
+	dev_t	devno = MKDEV(scull_major, scull_minor + index);
 
 	cdev_init(&dev->cdev, &scull_fops);
 	dev->cdev.owner = THIS_MODULE;
@@ -99,6 +279,7 @@ static int __init ft_larlenas_driver_init(void) {
 	while (i < scull_nr_devs) {
 		scull_devices[i].qset = scull_qset;
 		scull_devices[i].quantum = scull_quantum;
+		mutex_init(&scull_devices[i].mutex);
 		ft_scull_setup_cdev(&scull_devices[i], 0);
 		++i;
 	}
@@ -118,7 +299,9 @@ static void __exit ft_larlenas_driver_exit(void) {
 	printk(KERN_ALERT "Start the %s function\n", __FUNCTION__);
 	unregister_chrdev_region(devno, scull_nr_devs);
 	while (i < scull_nr_devs) {
+		ft_trim(&scull_devices[i]);
 		cdev_del(&scull_devices[i].cdev);
+		mutex_destroy(&scull_devices[i].mutex);
 		++i;
 	}
 	kfree(scull_devices);
