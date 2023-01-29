29de02cc4bc89045641c4eea26f78add1cd79c6c
larlena <azat251061@gmail.com>
2023-01-29 10:38:10 -0800

    fix wrong exception throw, increase buffer size, potentil segfault

--- Userspace/src/client/client.cc
+++ Userspace/src/client/client.cc
@@ -26,13 +26,16 @@ namespace {
 	}
 
 	std::string	read_from_standart_input() {
-		char		tmp[128];
+		char		tmp[1024];
+		size_t		tmp_size = sizeof(tmp) - 1;
 		std::string	msg;
-		int		ret_value = sizeof(tmp);
+		int		ret_value = tmp_size;
 		
-		while (ret_value == sizeof(tmp) && ((ret_value = read(STDIN_FILENO, tmp, sizeof(tmp))) > 0)) {
-			tmp[ret_value] = '\0';
-			msg += tmp;
+		while (	static_cast<size_t>(ret_value) == tmp_size
+			&& tmp[--ret_value] != '\n'
+			&& ((ret_value = read(STDIN_FILENO, tmp, tmp_size)) > 0)) {
+				tmp[ret_value] = '\0';
+				msg += tmp;
 		}
 		if (ret_value < 0) {
 			throw std::exception();
