53db2efe2d6e3a2516156c2f6e6398034eacac3b
larlena <azat251061@gmail.com>
2023-01-29 10:51:47 -0800

    deleted excess libraries

--- Userspace/src/server/server.hpp
+++ Userspace/src/server/server.hpp
@@ -2,20 +2,10 @@
 # define USERSPACE_SERVER_SERVER_HPP_
 
 # include <iostream>
-# include <queue>
-# include <exception>
-# include <vector>
-# include <thread>
-# include <memory>
-
 # include "../socket/server_socket_manager.hpp"
 
-
 namespace Server {
-
 	void	Start(const std::string &ip);
-
 }
-
 
 #endif // USERSPACE_SERVER_SERVER_HPP_
